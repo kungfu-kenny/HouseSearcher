16a138dd019e0831a10fdaa702b74252fd43eb21
kungfu-kenny <oleksandrshevchenko889@gmail.com>
2022-03-20 18:50:24 +0200

    started on workin on fifth website

--- parsing/parse_adress.py
+++ parsing/parse_adress.py
@@ -15,9 +15,19 @@ class ParseAdress(ParseMain):
     def __init__(self, driver_path:str, insert:str='', district:str='', rooms:list=[], price:int=0) -> None:
         super(ParseAdress, self).__init__(driver_path)
 
+    @staticmethod
+    def produce_list_rooms(rooms:list) -> list:
+        """
+        Static method which is dedicated to get the list of rooms
+        Input:  rooms = list of the rooms to use
+        Output: list of the rooms to use
+        """
+        return [str[room] for room in rooms]
 
     def produce_search_results(self) -> None:
         """
-        
+        Method which is dedicated to return the basic results to it
+        Input:  None
+        Output: we developed the full dataframe for all of it
         """
         pass
